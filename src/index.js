c93f33a511bb5b5b023a6e15d21d3cce8bb86305
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-08-08 09:56:55 -0300

    feat: add handler that perform simple get sending metrics to cloudwatch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-exports.handler = () => {
+const axios = require('axios');
+import cloudwatchMetricService from "./service/CloudwatchMetricService";
+import cloudwatchService from "./service/CloudwatchService";
 
+
+const SERVICE_URL = process.env.SERVICE_URL;
+const SERVICE_NAME = process.env.SERVICE_NAME;
+const SUCESS_METRIC_NAME = 'Sucess';
+const ERROR_METRIC_NAME = 'Error';
+const METRIC_UNIT = 'Count';
+const METRIC_VALUE = '';
+
+exports.handler = (event, context) => {
+    axios.get(SERVICE_URL)
+        .then(response => {
+            cloudwatchService.sendMetric(cloudwatchMetricService.buildCloudwatchServiceMonitoringMetric(
+                SUCESS_METRIC_NAME, METRIC_UNIT, METRIC_VALUE, SERVICE_NAME));
+        })
+        .catch(error => {
+            cloudwatchService.sendMetric(cloudwatchMetricService.buildCloudwatchServiceMonitoringMetric(
+                ERROR_METRIC_NAME, METRIC_UNIT, METRIC_VALUE, SERVICE_NAME));
+        });
 }
